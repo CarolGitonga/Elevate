a5441a0f54096824f4ffe204815c1727adfe995b
Carol Gitonga <=>
2023-03-23 18:48:18 +0300

    use restful web service to display json data

--- src/app/modules/product.module.ts
+++ src/app/modules/product.module.ts
@@ -1,13 +1,16 @@
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { Cart } from "../models/cart.model";
 import { Order } from "../models/order.model";
 import { OrderRepository } from "../models/order.repository";
 import { ProductRepository } from "../models/product.repository";
+import { RestDataSource } from "../models/rest.datasource";
 import { StaticDataSource } from "../models/static.datasource";
 
 @NgModule({
-    providers: [ProductRepository, StaticDataSource, Cart,
-                Order, OrderRepository]
+    imports: [HttpClientModule],
+    providers: [ProductRepository, Cart, Order, OrderRepository,
+      { provide: StaticDataSource, useClass: RestDataSource }]
 })
 export class ProductModule{}
 
